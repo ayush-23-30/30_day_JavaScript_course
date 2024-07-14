6d5fde5e7779f0072aecd9cfa60850e6cb9eb1b6
ayush-23-30 <pawar85060@gmail.com>
2024-07-14 11:52:22 +0530

    day 1

---
+++ DayOne/day1.js
@@ -0,0 +1,40 @@
+var a = 1;  
+console.log("this is value of Var variable " , a);
+let b = "ayush"; 
+console.log("this is value of let variable " , b);
+const c = true; 
+console.log("this is value of const variable " , c);
+ let arr = [1,2,3];
+console.log("this is value of Arr" , arr);
+let obj = {
+  name : "ayush ", 
+  age : 22
+
+}
+console.log("this is value of obj" , obj );
+
+
+
+console.log(typeof obj.name  , "data Type of object name ");
+console.log(typeof obj.age , "data type of object age ");
+console.log(typeof a ,  "data type of var A");
+console.log(typeof arr , "data type of let arr" );
+console.log(typeof c , "data type of const c");
+console.log(typeof b , "data type of b");
+
+// assigning a let variable n giving it a new value 
+
+let one = 12; 
+console.log("inital value of one" , one);
+
+one = "ayush bhai" ; 
+console.log("after reassign value of " , one);
+
+// try to reassgn value of const 
+
+const two = 23; 
+console.log("inital value of const two " , two );
+
+ // two = "ayush ";  // this is show a typeError beacuse you can't change the value of const  
+
+
