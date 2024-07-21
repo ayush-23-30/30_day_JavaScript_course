69427b9b5356f826cf2e9d73eb9eca1bcb642c7c
ayush-23-30 <pawar85060@gmail.com>
2024-07-21 11:32:28 +0530

    day 8 finished

--- day8/day8.js
+++ day8/day8.js
@@ -26,7 +26,7 @@ const {newName  = 12 , newAge = "pawar"} = obj
 
 let Arr  = [12,3,312,2]; 
 
-let newArr = [...Arr ,20,30,100  ]
+let newArr = [...Arr ,20,30,100 ]
 // console.log(newArr);
 
 
@@ -36,9 +36,52 @@ function restUsing (...num){
   return num.reduce((total, num) => total + num, 0)
 }
 
-console.log(restUsing(102,32,21,32,23,3));
+// console.log(restUsing(102,32,21,32,23,3));
 
+function defaultValue (a, b =2){
+  return a*b;
+}
 
+// console.log(defaultValue(10));
 
+function objectEnhanced (makeBy , model , year){
+  return {
+    makeBy , 
+    model, 
+    year
+  }
+}
+
+// console.log(objectEnhanced ("apple" , "macBook" ,"2022"));
+
+let name2 = "make"; 
+
+let i =0; 
+
+const laptop = {
+  [name2 + ++i] : "apple",
+  [name2 + ++i] : "dell", 
+  [name2 + ++i] : "hp"
+}
+
+// console.log(laptop.make1);
+// console.log(laptop.make2);
+// console.log(laptop.make3);
+
+let propName1 = "name"; 
+let propName2 = "age"; 
+let propName3 = "city";
+
+let propValue1 = "ayush";
+let propValue2 = "22";
+let propValue3 = "delhi";
+
+let obje = {
+  [propName1] : propValue1, 
+  [propName2] : propValue2, 
+  [propName3] : propValue3, 
+}
+
+console.log(obje);
 
 
