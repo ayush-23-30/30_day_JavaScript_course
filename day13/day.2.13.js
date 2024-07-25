cd9470eb86dc210464c8ef3990847e0338166811
ayush-23-30 <pawar85060@gmail.com>
2024-07-25 12:59:45 +0530

    this is day13

---
+++ day13/day.2.13.js
@@ -0,0 +1,10 @@
+// import addSum from './day13'
+// import {addSum , obj} from './day13'
+
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(addSum(10,2));
+
+import main from "./day13";
+
+console.log(main);
---
+++ day13/day13.js
@@ -0,0 +1,33 @@
+// const addExport = (a,b)=>{
+//   return a + b; 
+// }
+
+// const obj = {
+//   name : "ayush", 
+//   age : 22,
+//   ed : "MCA"
+// }
+
+// export default {addExport , obj} 
+// // this also target the task 3 , 4 
+// // module.exports = addExport;  
+
+
+function main (){
+  let aa = 10; 
+  let bb = 20; 
+
+console.log(aa + bb);
+
+function add (a , b){
+  return a+b; 
+
+}
+
+function multiple (a , b){
+  return a*b; 
+}
+
+}
+
+export default main; 
